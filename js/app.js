47565454ff4a26dec0b8504186230c56c19b59ce
Gerhard Riphagen <gerhard@studytube.nl>
2015-08-11 15:13:42 +0200

    Create product service, added buy button and placed product image in circle.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,46 @@
 var app = angular.module('app', []);
 
 
-app.controller('ProductController', ['$scope', function($scope) {
-    $scope.products = [
-        {
-            name: 'Coffee',
-            description: '10 cups of coffee @ Starbucks',
-            thumbnail: 'img/starbucks.jpg',
-            points: 1500
-        },
-        {
-            name: 'Cleaning',
-            description: '3 hours of cleaning from Helpling',
-            thumbnail: 'img/helpling.jpg',
-            points: 2000
-        },
-        {
-            name: '1 day off!',
-            description: 'Get an extra day of vacation',
-            thumbnail: 'img/vacation.jpeg',
-            points: 10000
+app.controller('ProductController', function($scope, ProductService) {
+    $scope.products = ProductService.getAll();
+});
+
+
+app.service('ProductService', function() {
+    this.getAll = function () {
+        return [
+            {
+                name: 'Coffee',
+                description: '10 cups of coffee @ Starbucks',
+                thumbnail: 'img/starbucks.jpg',
+                points: 1500
+            },
+            {
+                name: 'Cleaning',
+                description: '3 hours of cleaning from Helpling',
+                thumbnail: 'img/helpling.jpg',
+                points: 2000
+            },
+            {
+                name: '1 day off!',
+                description: 'Get an extra day of vacation',
+                thumbnail: 'img/vacation.jpeg',
+                points: 10000
+            }
+        ];
+    }
+});
+
+
+app.service('CurrentUserService', function() {
+    this.get = function () {
+        return {
+                firstName: 'Gerhard',
+                lastName: 'Riphagen',
+                moneyz: 2000
         }
-    ];
-}]);
+    }
+});
 
 
 app.directive('storeProduct', function() {
